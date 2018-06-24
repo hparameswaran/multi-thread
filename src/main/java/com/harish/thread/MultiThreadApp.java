d5e8ced10293b3f707d1533b96a7d753b5dd653f
Harish Parameswaran <Harish@apples-MacBook-Pro.local>
2018-06-23 21:40:53 -0400

    First commit

---
+++ src/main/java/com/harish/thread/MultiThreadApp.java
@@ -0,0 +1,13 @@
+package com.harish.thread;
+
+/**
+ * Hello world!
+ *
+ */
+public class MultiThreadApp 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Harish Ready For Some Multithreading !!!" );
+    }
+}
